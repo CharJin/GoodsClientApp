b638188af5e310eea0a748510f5bfdc602027597
CharJin <charjincser@gmail.com>
2019-04-10 22:17:09 +0800

    Complete simply the basic functions -- Sliding,TabLayout etc.

--- app/src/main/java/top/charjin/shoppingclient/LoginActivity.java
+++ app/src/main/java/top/charjin/shoppingclient/LoginActivity.java
@@ -1,13 +1,39 @@
 package top.charjin.shoppingclient;
 
 import android.os.Bundle;
+import android.support.design.widget.AppBarLayout;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 
+import top.charjin.shoppingclient.adapter.ViewPagerAdapter;
+import top.charjin.shoppingclient.fragment.ButtonFragment;
+import top.charjin.shoppingclient.fragment.TextFragment;
+
 public class LoginActivity extends AppCompatActivity {
+
+    private TabLayout tabLayout;
+    private AppBarLayout appBarLayout;
+    private ViewPager viewPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.tab_viewpaper);
+        tabLayout = findViewById(R.id.tablayout);
+        appBarLayout = findViewById(R.id.appbar_layout);
+
+        viewPager = findViewById(R.id.viewpager);
+        Fragment[] tabFgs = {new TextFragment(), new ButtonFragment()};
+        String[] tabFgTitles = {"Text", "Button"};
+
+        ViewPagerAdapter adapter = new ViewPagerAdapter(getSupportFragmentManager());
+        for (int i = 0; i < tabFgs.length; i++) {
+            adapter.addFragmentView(tabFgs[i], tabFgTitles[i]);
+        }
+        viewPager.setAdapter(adapter);
+        tabLayout.setupWithViewPager(viewPager);
+
     }
 }
---
+++ app/src/main/java/top/charjin/shoppingclient/utils/SharedPreferencesUtil.java
@@ -0,0 +1,17 @@
+package top.charjin.shoppingclient.utils;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+
+public class SharedPreferencesUtil {
+    public static boolean isFirstLaunched(Context context) {
+        SharedPreferences sp = context.getSharedPreferences("data", Context.MODE_PRIVATE);
+        boolean isFirst = sp.getBoolean("isFirst", true);
+        if (isFirst) {
+            SharedPreferences.Editor editor = sp.edit();
+            editor.putBoolean("isFirst", false);
+            editor.apply();
+        }
+        return isFirst;
+    }
+}
